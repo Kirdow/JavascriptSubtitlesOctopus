795457acc1dba1cfab2255cc497ca27924fe0de2
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2022-02-22 20:34:06 +0100

    this?

--- src/SubtitleOctopus.cpp
+++ src/SubtitleOctopus.cpp
@@ -175,7 +175,9 @@ public:
     /* CANVAS */
 
     void decodeBitmap(ASS_Image* img, ASS_Image** next) {
-        printf("Width and height: %d %d", img->w, img->h);
+        int a = img->w;
+        int b = img->h;
+        printf("Width and height: %d %d", a, b);
         if (img->w == 0 || img->h == 0) return;
         float a = (255 - (img->color & 255)) / 255.0;
         printf("Alpha %f", a);
